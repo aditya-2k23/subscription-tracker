dbfff49e603d400a580e960d43d6d56e4dee1b2f
aditya-2k23 <holaaditya123@gmail.com>
2025-03-11 16:27:06 +0530

    Add authorization middleware and subscription creation logic to routes

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -9,9 +11,7 @@ subscriptionRouter.get("/:id", (req, res) =>
   res.send({ title: "GET subscription details" })
 );
 
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE subscription" })
-);
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "UPDATE subscription" })
