fc13d4250bfe0f7366a6f080aafb2b3f024b50b5
aditya-2k23 <holaaditya123@gmail.com>
2025-03-12 14:25:56 +0530

    Add email sending functionality for subscription reminders
    Extracts email address to a constant
    
    Refactors email configuration by extracting the email address into a constant for better maintainability and reusability.
    
    Adds email sending functionality for subscription reminders

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 import { EMAIL_PASSWORD } from "./env.js";
 
-const transporter = nodemailer.createTransport({
+export const accountEmail = "holaaditya123@gmail.com";
+
+export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "holaaditya123@gmail.com",
+    user: accountEmail,
     pass: EMAIL_PASSWORD,
   },
 });
