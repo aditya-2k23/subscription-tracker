d0bf749bbabf9a3a08f1b7c76300ac9d64b1394a
aditya-2k23 <holaaditya123@gmail.com>
2025-03-12 14:28:49 +0530

    Adds email reminder functionality
    Integrates `sendReminderEmail` utility to trigger reminder emails
    Updates workflow to include email sending logic

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import { createRequire } from "module";
 import Subscription from "../models/subscription.model.js";
+import { sendReminderEmail } from "../utils/send-email.js";
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 
@@ -49,8 +50,12 @@ const sleepUntilReminder = async (context, label, date) => {
 };
 
 const triggerReminder = async (context, label) => {
-  return await context.run(label, () => {
+  return await context.run(label, async () => {
     console.log(`Triggering ${label} reminder...`);
-    // Send email or notification
+
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: reminder.label.subscription,
+    });
   });
 };
