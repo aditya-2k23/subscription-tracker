1445fc2f119807cc3242140e87d27bcbfc9ec3f2
aditya-2k23 <holaaditya123@gmail.com>
2025-03-12 14:28:39 +0530

    Refactor nodemailer transporter configuration and export default

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,10 +3,12 @@ import { EMAIL_PASSWORD } from "./env.js";
 
 export const accountEmail = "holaaditya123@gmail.com";
 
-export const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: accountEmail,
     pass: EMAIL_PASSWORD,
   },
 });
+
+export default transporter;
